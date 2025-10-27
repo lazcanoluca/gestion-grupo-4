cba8f21e11c9e1e76349a0a7574aa4e0011ffcc6
pataduras <frankf27810@gmail.com>
2025-10-26 22:31:13 -0300

    cambio de agregar materias 2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -211,7 +211,7 @@ function App() {
         </div>
       )}
 
-      {/* --- Modal de “Importar materias del SIU” --- */}
+      {/*  Modal de “Importar materias del SIU”  */}
       {showImportModal && (
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50">
           <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-md text-center">
